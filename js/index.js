c601446cb2e0e1c72b2d2c5fc1fa5afe7dd0523e
josiahmokob0 <jmnyarega@gmail.com>
2021-05-10 10:55:50 +0300

    slide

--- js/index.js
+++ js/index.js
@@ -1,30 +1,22 @@
 let current = 1;
+let previous = 2;
 let max = 2;
 let least = 1;
 
 const hide = (n) => {
-  const doc = document.querySelector(`.slide:nth-child(${n})`);
-  doc.classList.remove("display-none");
+  const doc = document.getElementById(`slide-${n + 1}`);
+  doc.style.display = "none";
 };
 
 const show = (n) => {
-  const doc = document.querySelector(`.slide:nth-child(${n})`);
-  doc.classList.add("display-block");
+  const doc = document.getElementById(`slide-${n + 1}`);
+  doc.style.display = "flex";
 };
 
 const next = () => {
-  current = (current % max) + 1;
-  previous = (current + 1) % max;
-  show(current);
-  hide(previous);
+  previous++;
   current++;
-};
 
-const prev = () => {
-  current = (current % max) + 1;
-  previous = (current + 1) % max;
-  show(current);
-  hide(previous);
-  console.log(current, previous);
-  current++;
+  show(current % max);
+  hide(previous % max);
 };
